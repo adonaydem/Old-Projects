b87ae7940b7c50b40c0d32f50219aa8919aa20dd
Adonay Demewez Gebremedhin <117765659+adonaydem@users.noreply.github.com>
2024-06-11 16:26:55 +0400

    Delete Job_up1 directory

--- Job_up1/transporter.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_GET['emeet'])){
-setcookie("emeet",$_GET['emeet'],time()+8640000, "/","", 0);
-session_start();
-$_SESSION['emeet']=$_GET['emeet'];
-header("Location: emeet.php");
-}
-?>
